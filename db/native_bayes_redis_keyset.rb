3815e554645d30f7766ccfcc8a8c66f5a00b9d0e
Andrew Curry <andrew.d.curry@gmail.com>
2012-02-20 13:22:25 -0500

    Made a module that describes a schema that NativeBayes uses with a redis db.

---
+++ db/native_bayes_redis_keyset.rb
@@ -0,0 +1,42 @@
+module NativeBayesRedisKeyset
+  DELIMITER = ":"
+  ROOT_KEY = "native_bayes"
+  WORDS_HASH_SUBKEY = "words"
+  CATEGORIES_DOCUMENTS_HASH_SUBKEY = "categories_documents"
+  CATEGORIES_WORDS_HASH_SUBKEY = "categories_words"
+  TOTAL_DOCUMENTS_SUBKEY = "total_documents"
+  TOTAL_WORDS_SUBKEY = "total_words"
+  THRESHOLD_SUBKEY = "threshold"
+  
+  def root_key
+    "#{ROOT_KEY}"
+  end
+  
+  def root_key_with_delimiter
+    "#{root_key}#{DELIMITER}"
+  end
+  
+  def words_hash_key
+    "#{root_key_with_delimiter}#{WORDS_HASH_SUBKEY}"
+  end
+  
+  def categories_documents_key
+    "#{root_key_with_delimiter}#{CATEGORIES_DOCUMENTS_HASH_SUBKEY}"
+  end
+  
+  def categories_words_key
+    "#{root_key_with_delimiter}#{CATEGORIES_WORDS_HASH_SUBKEY}"
+  end
+  
+  def total_documents_key
+    "#{root_key_with_delimiter}#{TOTAL_DOCUMENTS_SUBKEY}"
+  end
+  
+  def total_words_key
+    "#{root_key_with_delimiter}#{TOTAL_WORDS_SUBKEY}"
+  end
+  
+  def threshold_key
+    "#{root_key_with_delimiter}#{THRESHOLD_SUBKEY}"
+  end  
+end
